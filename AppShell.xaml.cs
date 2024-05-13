a4470cf331e7253939a2f1e156ad76773d05e0f2
Vinh Vu <53502920+vinhvu01@users.noreply.github.com>
2024-05-14 01:59:26 +0700

    First draft

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForecastMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
